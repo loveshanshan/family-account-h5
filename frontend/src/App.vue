1600aef875c521a60cb28dc564657d45a576d91e
loveshanshan <deng032ke@163.com>
2025-12-29 16:58:25 +0800

    Delete frontend directory

--- frontend/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup>
-// 这里不需要引入任何组件
-</script>
--- frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'tdesign-mobile-vue/es/style/index.css';
-
-createApp(App).use(router).mount('#app')
